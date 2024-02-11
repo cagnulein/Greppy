0814ee21fca3c8a5e10d0bde7791897ffe1be6f9
Roberto Viola <Cagnulein@gmail.com>
2024-02-11 09:08:21 +0100

    Initial Commit

---
+++ Greppy/GreppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreppyApp.swift
+//  Greppy
+//
+//  Created by Roberto Viola on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GreppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
