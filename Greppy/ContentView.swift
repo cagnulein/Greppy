db295fdc632d1bcbd896164746139e3c920ff878
Roberto Viola <Cagnulein@gmail.com>
2024-02-19 23:36:02 +0100

    Update ContentView.swift

--- Greppy/ContentView.swift
+++ Greppy/ContentView.swift
@@ -188,7 +188,7 @@ struct ContentView: View {
     
     func loadFileContent(from url: URL) -> String {
         searchTabs.removeAll()
-        addNewSearchTab(searchText: "")
+        //addNewSearchTab(searchText: "")
         // Assumi che questa funzione legga il contenuto del file e lo ritorni come String
         do {
             return try String(contentsOf: url)
