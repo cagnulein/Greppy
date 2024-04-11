6c5806536b5ce623e1d94f92c033efd867a2527a
Roberto Viola <roberto.viola@systemelectronics.com>
2024-04-11 08:31:18 +0200

    Update ContentView.swift

--- Greppy/ContentView.swift
+++ Greppy/ContentView.swift
@@ -377,9 +377,19 @@ struct ContentView: View {
         bookmarkedLines = []
         searchTabs.removeAll()
         addNewSearchTab(searchText: "")
-        // Assumi che questa funzione legga il contenuto del file e lo ritorni come String
+
         do {
-            return try String(contentsOf: url, encoding: .utf8)
+            let fileData = try Data(contentsOf: URL(fileURLWithPath: url))
+        
+            var encoding: String.Encoding = .utf8 // Encoding predefinito
+            
+            // Prova a rilevare l'encoding
+            let detectedEncoding = NSString.stringEncoding(for: fileData, encodingOptions: nil, convertedString: nil, usedLossyConversion: nil)
+            if detectedEncoding != 0 {
+                encoding = String.Encoding(rawValue: detectedEncoding)
+            }
+            
+            return try String(contentsOf: url, encoding: encoding)
         } catch {
             print("Errore nella lettura del file: \(error)")
             return "Errore nella lettura del file \(error.localizedDescription)"
